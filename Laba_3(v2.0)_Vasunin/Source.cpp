5a2c94f30f47834cd6a5bd573b86c0a1832b1ed5
Nesterov Vlad <nesterovvlad100@gmail.com>
2021-12-09 12:49:00 +0300

    deleted useless comments

--- Laba_3(v2.0)_Vasunin/Source.cpp
+++ Laba_3(v2.0)_Vasunin/Source.cpp
@@ -7,40 +7,21 @@ string x = "1221";
 
 
 bool IsPolindrom(string x) {
-	//int c = 0;
-
-	//if (x.length()%2 !=0)
-	//{
-	//	c = (x.length() / 2);
-		
-	//}
-	//else
-	//{
-	//int	c = x.length() / 2;
-	//}
 
 	for (int i = 0; i < (x.length() / 2); i++)
 	{
 		if (x[i] == x[x.length() - i - 1])
 		{
 			
-			//c++;
-			//cout << c << endl;
+			
 		}
 		else
 		{
 			return false;
 		}
 	}
-	//if (c == sizeof(x)%2)
-	//{
+	
 		return true;
-	//}
-	//else
-	//{
-	//	return 0;
-	//}
-
 }
 
 
@@ -50,7 +31,7 @@ int main() {
 	cout << "vvedite 4islo" << endl;
 	cin >> x ;
 	cout << endl;
-	//IsPolindrom(x);
+
 	if (IsPolindrom(x))
 	{
 		cout << "Polindrom" << endl;
